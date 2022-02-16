234f724439d4640485babe0e9020b0978f9663cf
vuhoanghuy <huy.vh1680@sinhvien.hoasen.edu.vn>
2022-02-16 17:03:45 +0700

    Nop bai

---
+++ D9/Huy/src/app/service/payment.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { DataService } from './data.service'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+
+  constructor(public dtS:DataService ) { }
+  // for (let i=0; i<=this.dtS.DataService.price){
+  }
+
+  
+
