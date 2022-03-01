6a19e431230cc9f3e9521c56c80317a89ba20947
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-03-01 23:51:45 +0700

    Hao D21 Two

--- D21/Hao-WebbanGame/src/environments/environment.ts
+++ D21/Hao-WebbanGame/src/environments/environment.ts
@@ -5,8 +5,10 @@
 export const environment = {
   firebase: {
     projectId: 'webbangame',
-    appId: '1:194316627686:web:099586324c41bac7531527',
+    appId: '1:194316627686:web:7165a5b7f6eb66e9531527',
+    databaseURL: 'https://webbangame-default-rtdb.asia-southeast1.firebasedatabase.app',
     storageBucket: 'webbangame.appspot.com',
+    locationId: 'asia-southeast1',
     apiKey: 'AIzaSyBtELE5bGNfZ1bnXsiH1fDJXvH8rJ_SPAc',
     authDomain: 'webbangame.firebaseapp.com',
     messagingSenderId: '194316627686',
